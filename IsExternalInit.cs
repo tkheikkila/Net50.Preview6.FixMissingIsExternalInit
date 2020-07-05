2a98078f281cf5f14fee73a1252ea61e8cf179d3
Toni Heikkilä <toni.heikkila@protonmail.com>
2020-07-05 17:26:16 +0300

    Add project files.

---
+++ IsExternalInit.cs
@@ -0,0 +1,15 @@
+﻿#if NETCOREAPP5_0
+
+using System.ComponentModel;
+using System.Diagnostics;
+
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    [DebuggerStepThrough, DebuggerNonUserCode, EditorBrowsable(EditorBrowsableState.Never)]
+    // ReSharper disable once UnusedMember.Global
+    public static class IsExternalInit { }
+
+}
+#endif
